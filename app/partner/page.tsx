3aa3bdbf9c2504b355635220daa5f7c298f4cd16
Fazil <136937292+MuhammedFazilPc@users.noreply.github.com>
2024-09-20 14:24:10 +0530

    Update page.tsx

--- app/partner/page.tsx
+++ app/partner/page.tsx
@@ -117,3 +117,15 @@
 // };
 
 // export default WorkingPartnerForm;
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <h3>coming soon</h3>
+    </div>
+  );
+}
+
+export default page;
+
